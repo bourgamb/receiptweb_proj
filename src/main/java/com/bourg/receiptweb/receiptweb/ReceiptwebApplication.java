886816b5eaf668c7e3dc02931d9879eb540b5a24
bourgamb <ambrose.bourg@gmail.com>
2020-11-11 12:16:55 +0000

    Initial Commit

---
+++ src/main/java/com/bourg/receiptweb/receiptweb/ReceiptwebApplication.java
@@ -0,0 +1,13 @@
+package com.bourg.receiptweb.receiptweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bourg/receiptweb/receiptweb/ReceiptwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bourg.receiptweb.receiptweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
